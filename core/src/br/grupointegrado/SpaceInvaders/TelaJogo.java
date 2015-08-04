76b7aa712239c26b4575482e7dcb59fb2a7424e9
laydi <laydi.nay@hotmail.com>
2015-08-03 21:09:49 -0300

    Criado as tela Base e Jogo, e modificado a tela MainGame.

---
+++ core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
@@ -0,0 +1,110 @@
+package br.grupointegrado.SpaceInvaders;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+
+/**
+ * Created by Laydi on 03/08/2015.
+ */
+public class TelaJogo extends TelaBase{
+
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private Stage palco;
+    private BitmapFont fonte;
+    private Label lbPontuacao;
+
+
+    /**
+     * construtor padrao da tela de jogo
+     * @param game referencia para a classe principal
+     */
+    public TelaJogo(MainGame game) {
+        super(game);
+    }
+
+    /**
+     * chamado quando a tela é exibida
+     */
+    @Override
+    public void show() {
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch = new SpriteBatch();
+        palco = new Stage(new FillViewport(camera.viewportWidth, camera.viewportHeight, camera));
+
+        initFonte();
+        initInformacoes();
+    }
+
+    private void initFonte(){
+        fonte = new BitmapFont();
+    }
+
+    private void initInformacoes(){
+        Label.LabelStyle lbEstilo = new Label.LabelStyle();
+        lbEstilo.font = fonte;
+        lbEstilo.fontColor = Color.WHITE;
+
+        lbPontuacao = new Label("0 pontos", lbEstilo);
+        palco.addActor(lbPontuacao);
+    }
+
+    /**
+     * chamado a todo quadro de atualização do jogo (FPS)
+     * @param delta tempo entre um quadro e outro (em segundos)
+     */
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(.15f, .15f, .25f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        lbPontuacao.setPosition(10, camera.viewportHeight - 20);
+
+        palco.act(delta);
+        palco.draw();
+    }
+
+    /**
+     * chamado sempre que ha uma alteração no tamanho da tela
+     * @param width novo valor de largura da tela
+     * @param height novo valor de altura da tela
+     */
+    @Override
+    public void resize(int width, int height) {
+        camera.setToOrtho(false, width, height);
+        camera.update();
+    }
+
+    /**
+     * chamado sempre que o jogo for minimizado
+     */
+    @Override
+    public void pause() {
+
+    }
+
+    /**
+     * chamado sempre que o jogo voltar para o primeiro plano
+     */
+    @Override
+    public void resume() {
+
+    }
+
+    /**
+     * chamado quando a tela for destruida
+     */
+    @Override
+    public void dispose() {
+        batch.dispose();
+        palco.dispose();
+        fonte.dispose();
+    }
+}

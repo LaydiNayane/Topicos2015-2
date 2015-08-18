10856169e24b58379caeb6ebcdd83f2327364e4d
laydi <laydi.nay@hotmail.com>
2015-08-17 21:08:33 -0300

    Inserção da nave, movimento para os lados; Inserção dos tiros; Inserção dos meteoros.

--- core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
+++ core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
@@ -1,13 +1,20 @@
 package br.grupointegrado.SpaceInvaders;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 
 /**
@@ -20,7 +27,21 @@ public class TelaJogo extends TelaBase{
     private Stage palco;
     private BitmapFont fonte;
     private Label lbPontuacao;
-
+    private Image jogador;
+    private Texture texturaJogador;
+    private Texture texturaJogadorDireita;
+    private Texture texturaJogadorEsquerda;
+    private boolean indoDireita;
+    private boolean indoEsquerda;
+    private boolean indoCima;
+    private boolean indoBaixo;
+    private boolean atirando;
+    private Array<Image> tiros = new Array<Image>();
+    private Texture texturaTiros;
+    private Texture texturaMeteoro1;
+    private Texture texturaMeteoro2;
+    private Array<Image> meteoros1 = new Array<Image>();
+    private Array<Image> meteoros2 = new Array<Image>();
 
     /**
      * construtor padrao da tela de jogo
@@ -41,11 +62,33 @@ public class TelaJogo extends TelaBase{
 
         initFonte();
         initInformacoes();
+        initJogador();
+        initTexturas();
+    }
+
+    private void initTexturas() {
+        texturaTiros = new Texture("sprites/shot.png");
+        texturaMeteoro1 = new Texture("sprites/enemie-1.png");
+        texturaMeteoro2 = new Texture("sprites/enemie-2.png");
     }
 
     private void initFonte(){
         fonte = new BitmapFont();
     }
+
+    private void initJogador() {
+        texturaJogador = new Texture("sprites/player.png");
+        texturaJogadorDireita = new Texture("sprites/player-right.png");
+        texturaJogadorEsquerda = new Texture("sprites/player-left.png");
+
+        jogador = new Image(texturaJogador);
+        float x = camera.viewportWidth / 2 - jogador.getWidth() / 2;
+        float y = 10;
+        jogador.setPosition(x, y);
+        palco.addActor(jogador);
+    }
+
+
 
     private void initInformacoes(){
         Label.LabelStyle lbEstilo = new Label.LabelStyle();
@@ -66,9 +109,147 @@ public class TelaJogo extends TelaBase{
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         lbPontuacao.setPosition(10, camera.viewportHeight - 20);
+        capturaTeclas();
+        atualizarJogador(delta);
+        atualizarTiros(delta);
+        atualizarMeteoros(delta);
 
         palco.act(delta);
         palco.draw();
+    }
+
+    private void atualizarMeteoros(float delta) {
+        int tipo = MathUtils.random(1, 3);
+
+        if(tipo == 1){
+            // cria meteoro 1
+            Image meteoro = new Image(texturaMeteoro1);
+            float x = MathUtils.random(0, camera.viewportWidth - meteoro.getWidth());
+            float y = MathUtils.random(camera.viewportHeight, camera.viewportHeight * 2);
+            meteoro.setPosition(x, y);
+            meteoros1.add(meteoro);
+            palco.addActor(meteoro);
+        }else{
+            // cria meteoro 2
+        }
+        float velocidade = 200;
+        for(Image meteoro : meteoros1){
+            float x = meteoro.getX();
+            float y = meteoro.getY() - velocidade * delta;
+            meteoro.setPosition(x, y);
+        }
+    }
+
+    private float intervaloTiros = 0; // tempo acumulado entre os tiros
+    private final float MIN_INTERVALO_TIROS = 0.4f; // minimo de tempo entre os tiros
+
+    private void atualizarTiros(float delta) {
+        // cria um novo tiro se necessario
+        intervaloTiros = intervaloTiros + delta; // acumula o tempo percorrido
+        if(atirando){
+            if(intervaloTiros >= MIN_INTERVALO_TIROS){ // verifica se o tempo minimo foi atingido
+                Image tiro = new Image(texturaTiros);
+                float x = jogador.getX() + jogador.getWidth() / 2 - tiro.getWidth() / 2;
+                float y = jogador.getY() + jogador.getHeight();
+                tiro.setPosition(x, y);
+                tiros.add(tiro);
+                palco.addActor(tiro);
+                intervaloTiros = 0;
+            }
+
+        }
+        float velocidade = 200; // velocidade de movimentacao do tiro
+        for(Image tiro : tiros){ // percorre todos os tiros existentes
+            //movimenta o tiro em direcao ao topo
+            float x = tiro.getX();
+            float y = tiro.getY() + velocidade * delta;
+            tiro.setPosition(x, y);
+            //remove os tiros que sairam de posicao da tela
+            if(tiro.getY() > camera.viewportHeight){
+                tiros.removeValue(tiro, true); // remove da lista
+                tiro.remove();// remove do palco
+            }
+        }
+    }
+
+    /**
+     * Atualiza a posicao do jogador
+     * @param delta
+     */
+    private void atualizarJogador(float delta) {
+        float velocidade = 200; //Velocidade de movimento do jogador
+        if (indoDireita) {
+
+            if (jogador.getX() < camera.viewportWidth - jogador.getWidth()) {
+                float x = jogador.getX() + velocidade * delta;
+                float y = jogador.getY();
+                jogador.setPosition(x, y);
+
+            }
+        }
+        if (indoEsquerda) {
+            if (jogador.getX() > 0) {
+                float x = jogador.getX() - velocidade * delta;
+                float y = jogador.getY();
+                jogador.setPosition(x, y);
+
+            }
+        }
+        if (indoCima) {
+            float x = jogador.getX();
+            float y = jogador.getY() + velocidade * delta;
+            jogador.setPosition(x, y);
+
+        }
+
+        if (indoBaixo) {
+            float x = jogador.getX();
+            float y = jogador.getY() - velocidade * delta;
+            jogador.setPosition(x, y);
+
+
+        }
+        if (indoDireita) {
+            //trocar imagem direita
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturaJogadorDireita)));
+        }else if (indoEsquerda) {
+            //trocar imagem esqueda
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturaJogadorEsquerda)));
+        }else {
+            //trocar imagem cena
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturaJogador)));
+        }
+
+    }
+
+    /**
+     * Verifica se as teclas estao pressionadas
+     */
+    private void capturaTeclas() {
+        indoDireita = false;
+        indoEsquerda = false;
+        //indoCima = false;
+        //indoBaixo = false;
+        atirando = false;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            indoEsquerda = true;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            indoDireita = true;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            atirando = true;
+        }
+
+
+        //if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+          //  indoCima = true;
+        //}
+        //if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+          //  indoBaixo = true;
+        //}
     }
 
     /**
@@ -106,5 +287,12 @@ public class TelaJogo extends TelaBase{
         batch.dispose();
         palco.dispose();
         fonte.dispose();
+        texturaJogador.dispose();
+        texturaJogadorDireita.dispose();
+        texturaJogadorEsquerda.dispose();
+        texturaTiros.dispose();
+        texturaMeteoro1.dispose();
+        texturaMeteoro2.dispose();
+
     }
 }

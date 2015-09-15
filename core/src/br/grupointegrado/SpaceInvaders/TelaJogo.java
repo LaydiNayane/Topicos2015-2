6ef3d8f47971dbe0153064c38efcf210b3c3553a
laydi <laydi.nay@hotmail.com>
2015-09-14 21:02:27 -0300

    Adicionado audio nas ações de tiros, explosões e game over, criação da tela de menu e alteração da tela inicial do jogo.

--- core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
+++ core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
@@ -2,6 +2,8 @@ package br.grupointegrado.SpaceInvaders;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -50,6 +52,11 @@ public class TelaJogo extends TelaBase{
     private Array<Texture> texturasExplosao = new Array<Texture>();
     private Array<Explosao> explosoes = new Array<Explosao>();
 
+    private Sound somTiro;
+    private Sound somExplosao;
+    private Sound somGameOver;
+    private Music musicaFundo;
+
     /**
      * construtor padrao da tela de jogo
      * @param game referencia para a classe principal
@@ -72,7 +79,16 @@ public class TelaJogo extends TelaBase{
         initInformacoes();
         initJogador();
         initTexturas();
+        initSons();
 
+    }
+
+    private void initSons() {
+        somTiro = Gdx.audio.newSound(Gdx.files.internal("sounds/shoot.mp3"));
+        somExplosao = Gdx.audio.newSound(Gdx.files.internal("sounds/explosion.mp3"));
+        somGameOver = Gdx.audio.newSound(Gdx.files.internal("sounds/gameover.mp3"));
+        musicaFundo = Gdx.audio.newMusic(Gdx.files.internal("sounds/background.mp3"));
+        musicaFundo.setLooping(true);
     }
 
     private void initTexturas() {
@@ -146,12 +162,17 @@ public class TelaJogo extends TelaBase{
         atualizarExplosoes(delta);
 
         if(gameOver == false){
+            if (!musicaFundo.isPlaying())
+                musicaFundo.play();
             capturaTeclas();
             atualizarJogador(delta);
             atualizarTiros(delta);
             atualizarMeteoros(delta);
             detectarColisoes(meteoros1, 5);
             detectarColisoes(meteoros2, 5);
+        }else {
+            if (musicaFundo.isPlaying())
+                musicaFundo.stop();
         }
 
         palco.act(delta);// atualiza situacao do palco
@@ -195,7 +216,7 @@ public class TelaJogo extends TelaBase{
                     tiros.removeValue(tiro, true);//remove da lista
                     meteoro.remove();//remove do palco
                     meteoros.removeValue(meteoro, true);//remove da lista
-                    criarExplosao(meteoro.getX(), meteoro.getY());
+                    criarExplosao(meteoro.getX() + meteoro.getWidth() / 2, meteoro.getY() + meteoro.getHeight() / 2);
                 }
             }
 
@@ -203,6 +224,7 @@ public class TelaJogo extends TelaBase{
             if (recJogador.overlaps(recMeteoro)){
                 //ocorre colisao de jogador com meteoro 1
                 gameOver = true;
+                somGameOver.play();
             }
         }
 
@@ -211,11 +233,12 @@ public class TelaJogo extends TelaBase{
     // cria a explosao na posicao x e y
     private void criarExplosao(float x, float y) {
         Image ator = new Image(texturasExplosao.get(0));
-        ator.setPosition(x, y);
+        ator.setPosition(x - ator.getWidth() / 2, y - ator.getHeight() / 2);
         palco.addActor(ator);
 
         Explosao explosao = new Explosao(ator, texturasExplosao);
         explosoes.add(explosao);
+        somExplosao.play();
     }
 
     private void atualizarMeteoros(float delta) {
@@ -284,6 +307,7 @@ public class TelaJogo extends TelaBase{
                 tiros.add(tiro);
                 palco.addActor(tiro);
                 intervaloTiros = 0;
+                somTiro.play();
             }
 
         }
@@ -426,6 +450,10 @@ public class TelaJogo extends TelaBase{
         for (Texture text : texturasExplosao){
             text.dispose();
         }
+        somExplosao.dispose();
+        somTiro.dispose();
+        somGameOver.dispose();
+        musicaFundo.dispose();
 
     }
 }
